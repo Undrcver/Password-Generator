b7cdc130d578476ae4bc977f2a1d1bd30b4fb114
Louie <Loualicea@hotmail.com>
2023-02-22 10:26:05 -0500

    updated script error messages

--- script.js
+++ script.js
@@ -9,28 +9,27 @@ var generateBtn = document.querySelector("#generate");
 // Password 
 function generatePassword() {
     // ask user for number of characters
-  var userChoice = window.prompt ('How many characters would you like you password to contain?');
-  // if user hit cancel end 
+  var userChoice = window.prompt ('How many letters would you like the password to contain?'); 
   if (!userChoice) {
     return;
   };
-  // if the user wants a certain number of characters userchoice parseInt()
+  
   userChoice = parseInt(userChoice);
   numberOfChars = userChoice;
   // set character limits
   if (numberOfChars < 8 || numberOfChars > 128) {
-    window.alert(' Oh no! Password must be a minimum of 8 characters and maximum of 128 characters, Please try again.');
+    window.alert(' Password must be a minimum of 8 characters Please try again.');
   };
 
   // ask user if the want uppercase chars? 
-  var getUpperCase = window.confirm('Would you like to incorporate uppercase letters?');
+  var getUpperCase = window.confirm('Would you like some uppercase letters?');
   // ask user if the want lowercase chars? 
-  var lowerCase =  window.confirm('Would you like to incorporate lowercase letters?');
+  var lowerCase =  window.confirm('Would you like some lowercase letters?');
    // ask user if the want numbs? 
-  var confirmNumbs =  window.confirm('Would you like to incorporate numbers?');
+  var confirmNumbs =  window.confirm('Would you like to add some numbers?');
   // ask user if the want special chars? 
-  var confirmSpecialChars = window.confirm('Would you like to incorporate special characters?');
-  // if user wants special chars, numbs, lower or upper case randomize those chars. 
+  var confirmSpecialChars = window.confirm('Would you like to add some special characters?');
+ 
   if (getUpperCase === true) {
     getUpperCase = Math.floor(Math.random()* upperChars.length +1 ); 
     generatedPassword = generatedPassword.concat(upperChars[getUpperCase]);
